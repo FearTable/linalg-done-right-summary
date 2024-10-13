024c27ea7c721623cae2bda24e97d3df2fb8c455
FearTable <michelwerder@gmail.com>
2024-10-13 15:59:05 +0200

    added some octave code, changed semi-boldness

---
+++ octave/column_wise_matrix_mult.m
@@ -0,0 +1,16 @@
+function retval = column_wise_matrix_mult (C,R)
+  m = rows(C);
+  c = columns(C); % = rows(R)
+  n = columns (R);
+  retval=zeros(m,n);
+
+  if (columns(C) != rows(R))
+    error('columns of C and rows of R dont match');
+  end
+
+  for (j=1:m)
+    for (l=1:c)
+      retval(j,:)=retval(j,:)+C(j,l)*R(l,:);
+    end
+  end
+end
---
+++ octave/row_wise_matrix_mult.m
@@ -0,0 +1,16 @@
+function retval = row_wise_matrix_mult (C,R)
+  m = rows(C);
+  c = columns(C); % = rows(R)
+  n = columns (R);
+  retval=zeros(m,n);
+
+  if (columns(C) != rows(R))
+    error('columns of C and rows of R dont match');
+  end
+
+  for (k=1:n)
+    for (i=1:c)
+      retval(:,k)=retval(:,k)+C(:,i)*R(i,k);
+    end
+  end
+end
